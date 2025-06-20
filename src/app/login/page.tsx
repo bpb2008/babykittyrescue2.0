3df27b346f23c87fc7ee5076f351f92a61b49072
bpb2008 <brittanythompson08@gmail.com>
2025-06-19 20:15:23 -0500

    deleted auth api route

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,14 @@
-"use client";
+// "use client";
 
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
 
-export default function LoginPage() {
-  return (
-    <button
-      className="border border-[#DCCAE3] bg-[#221a20] text-white px-4 py-2 rounded hover:bg-[#DCCAE3] hover:text-black transition-colors duration-300"
-      onClick={() => signIn("google")}
-    >
-      Admin Login
-    </button>
-  );
-}
+// export default function LoginPage() {
+//   return (
+//     <button
+//       className="border border-[#DCCAE3] bg-[#221a20] text-white px-4 py-2 rounded hover:bg-[#DCCAE3] hover:text-black transition-colors duration-300"
+//       onClick={() => signIn("google")}
+//     >
+//       Admin Login
+//     </button>
+//   );
+// }
